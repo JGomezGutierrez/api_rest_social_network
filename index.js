7211ad384e4bfc42981fea9c173df4fff45626b7
Juliet Gomez <yuligomg@gmail.com>
2024-06-11 20:59:42 -0500

    initial project and start-project.bat

---
+++ index.js
@@ -0,0 +1,13 @@
+//Importaciones
+
+
+// Conexion a la base de datos 
+
+// Configurar cors: permite que las peticiones se hagan directamente
+
+
+// Conversion de datos (body a objetos JS)
+
+//Configurar rutas
+
+//Configurar el servidor para escuchar las peticiones HTTP 
