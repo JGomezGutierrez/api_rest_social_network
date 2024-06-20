846ad5b42d076677443c6ac53e24a9b999c24894
Juliet Gomez <yuligomg@gmail.com>
2024-06-19 22:03:29 -0500

    updated followers

--- routes/follow.js
+++ routes/follow.js
@@ -1,10 +1,13 @@
 //importaciones
 import { Router } from "express";
 const router = Router();
-import { testFollow } from "../controllers/follow.js"
+import { saveFollow, testFollow } from "../controllers/follow.js"
+import { ensureAuth } from "../middlewares/authentication.js";
 
 //Definir la ruta .
 router.get('/test-follow', testFollow);
+router.post("/follow", ensureAuth, saveFollow);
+
 
 //Exportar el Router
 export default router;
